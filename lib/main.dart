36929f7e411155a3077ed1576332b3a9c3a3463c
JosephineAkello <josephineakello2030@gmail.com>
2021-07-02 21:32:16 +0300

    add intentional errors to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'secretes.dart';
 
 Future<void> main() async {
   await SentryFlutter.init(
-    (options) => options.dsn = 'https://your sentry.io DSN',
+    (options) => options.dsn =
+    //add your DSN here
+        '$myDsn',
     appRunner: () => runApp(MyApp()),
-    );
+  );
 }
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primaryColor: Colors.green,
         ),
         home: Scaffold(
-          appBar: AppBar(title: Text('Flutter Sentry Test'), centerTitle: true),
-        ));
+            appBar:
+                AppBar(title: Text('Flutter Sentry Test'), centerTitle: true),
+            body: Center(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green)),
+                child: Text('I Am  buggy'),
+                onPressed: () {
+                  throw new StateError('This is a Dart exception error.');
+                },
+              ),
+            )));
   }
 }
